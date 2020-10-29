bc218bf0dfb6c352791b179a5b347770196f3239
PereverzevGlebb <peglebreverzev@gmail.com>
2020-10-29 14:40:32 +0200

    first change

--- src/main/java/com/example/demo/Controllers/GreetingController.java
+++ src/main/java/com/example/demo/Controllers/GreetingController.java
@@ -2,11 +2,12 @@ package com.example.demo.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class GreetingController {
-
+    @GetMapping("/")
     public String mainPage(Model model){
      return "main";
  }
