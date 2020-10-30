bebf41a3883475426aa476145671c7ae7ff9500d
PereverzevGlebb <peglebreverzev@gmail.com>
2020-10-30 23:07:31 +0200

    +Getters

--- src/main/java/com/example/demo/Models/Post.java
+++ src/main/java/com/example/demo/Models/Post.java
@@ -4,9 +4,46 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+
 @Entity
 public class Post {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long id;
+    private String NameOfProduct;
+    private float Coast;
+    private String DescriptionOfProduct;
+
+    public float getCoast() {
+        return Coast;
+    }
+
+    public void setCoast(float coast) {
+        Coast = coast;
+    }
+
+    public String getDescriptionOfProduct() {
+        return DescriptionOfProduct;
+    }
+
+    public void setDescriptionOfProduct(String descriptionOfProduct) {
+        DescriptionOfProduct = descriptionOfProduct;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNameOfProduct() {
+        return NameOfProduct;
+    }
+
+    public void setNameOfProduct(String nameOfProduct) {
+        NameOfProduct = nameOfProduct;
+    }
 }
