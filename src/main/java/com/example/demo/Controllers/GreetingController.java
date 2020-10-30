dfe0a14ee726246c6846cc002ff0c9467c551c71
PereverzevGlebb <peglebreverzev@gmail.com>
2020-10-30 22:20:27 +0200

    +update

--- src/main/java/com/example/demo/Controllers/GreetingController.java
+++ src/main/java/com/example/demo/Controllers/GreetingController.java
@@ -1,5 +1,7 @@
 package com.example.demo.Controllers;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GreetingController {
+
+
     @GetMapping("/")
     public String mainPage(Model model){
-     return "mainPage";
+
+        return "mainPage";
  }
 
 }
