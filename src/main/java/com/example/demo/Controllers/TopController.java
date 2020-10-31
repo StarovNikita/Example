a015d73bbceea3cae394be162efdfce4aaa878ec
Никита Старов <starovnikita15@gmail.com>
2020-10-31 20:59:16 +0200

    dataBase work

--- src/main/java/com/example/demo/Controllers/TopController.java
+++ src/main/java/com/example/demo/Controllers/TopController.java
@@ -4,25 +4,20 @@ import com.example.demo.Models.Post;
 import com.example.demo.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(path="/demo")
+
 class TopController {
 
     @Autowired
     private PostRepository postRepository;
     @PostMapping(path="/add")
-    public @ResponseBody String addNewUser (@RequestParam String NameOfProduct, @RequestParam float Cost) {
-        Post n = new Post();
-        n.setNameOfProduct(NameOfProduct);
-        n.setCoast(Cost);
-        postRepository.save(n);
-        return "Saved";
-    }
-    @GetMapping(path="/all")
-    public @ResponseBody Iterable<Post> getAllUsers() {
-        // This returns a JSON or XML with the users
-        return postRepository.findAll();
+    public  String mainPage (Model model) {
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts",posts);
+        return "mainPage";
+
     }
 }
