da8d34f320fa6f5106498b49bad09720c9cb93ea
PereverzevGlebb <peglebreverzev@gmail.com>
2020-11-04 00:14:44 +0200

    +hibernate depend

--- src/main/java/com/example/demo/Controllers/GreetingController.java
+++ src/main/java/com/example/demo/Controllers/GreetingController.java
@@ -3,7 +3,6 @@ package com.example.demo.Controllers;
 
 import com.example.demo.Models.Post;
 import com.example.demo.repository.PostRepository;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
