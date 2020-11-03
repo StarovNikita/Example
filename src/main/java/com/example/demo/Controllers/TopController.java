cd94896371c57c0bf66c5015d0287a70c17b86ea
PereverzevGlebb <peglebreverzev@gmail.com>
2020-11-03 23:23:29 +0200

    new properties + new dep-ies in pom.xml

---
+++ src/main/java/com/example/demo/Controllers/TopController.java
@@ -0,0 +1,23 @@
+package com.example.demo.Controllers;
+
+
+import com.example.demo.Models.Post;
+import com.example.demo.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TopController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping("/mainPage")
+    public String mainPage(Model model) {
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
+        return "mainPage";
+    }
+}
