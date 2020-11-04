a1d68960f8509e4ea6f8a1442115ea942ceed14a
Никита Старов <starovnikita15@gmail.com>
2020-11-04 23:54:35 +0200

    work Web

--- src/main/java/com/example/demo/Controllers/TopController.java
+++ src/main/java/com/example/demo/Controllers/TopController.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class Catalog{
-    @GetMapping ("/catalogPage")
-    public  String catalogPage(Model model) {return "/catalogPage";}
+public class TopController {
+    @GetMapping("/")
+
+    public String mainPage(Model model){
+        return "mainPage";
+    }
 }
